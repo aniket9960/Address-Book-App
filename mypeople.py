de5445963f724d3e07c5f69540fd46a4691ec606
aniket9960 <aniketjagtap9960@gmail.com>
2021-11-16 18:26:23 +0530

    Update person window completed

--- mypeople.py
+++ mypeople.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import Toplevel
 import sqlite3
-from tkinter import font
+from tkinter import messagebox
 import addpeople
 con = sqlite3.connect('database.db')
 cur = con.cursor()
@@ -38,7 +38,7 @@ class MyPeople(Toplevel):
         self.sb.grid(row=0,column=1,sticky=N+S)
 
         persons=cur.execute("select * from persons").fetchall()
-        print(persons)
+        
         count = 0
         for person in persons:
             
@@ -70,9 +70,9 @@ class MyPeople(Toplevel):
         selected = self.listbox.curselection()
         person = self.listbox.get(selected)
         person_id = person.split("-")[0]
-        print(person_id)
         
         updatepage = Update()
+        
 
     
 
@@ -87,7 +87,6 @@ class Update(Toplevel):
         global person_id
         person = cur.execute("select * from persons where person_id = ?",(person_id,))
         person_info = person.fetchall()
-        print(person_info)
         self.id = person_info[0][0]
         self.name = person_info[0][1]
         self.surname = person_info[0][2]
@@ -95,5 +94,81 @@ class Update(Toplevel):
         self.phone = person_info[0][4]
         self.address = person_info[0][5]
 
-        
+        #frames.......
+        self.top=Frame(self,height=150,bg='white')
+        self.top.pack(fill=X)
 
+        self.bottom=Frame(self,height=600,bg='#fcc324')
+        self.bottom.pack(fill=X)
+
+        #Heading........
+        self.top_Image=PhotoImage(file='icons/addperson.png')
+        self.top_Image_Label = Label(self.top,image=self.top_Image,bg='white')
+        self.top_Image_Label.place(x=120,y=10)
+        self.heading = Label(self.top,text="Add Persons",font='arial 15 bold',fg='#003f8a',bg='white')
+        self.heading.place(x=260,y=60)
+        
+        #Scrollbar....
+        self.sb = Scrollbar(self.bottom,orient=VERTICAL)
+
+
+        #Name
+        self.lbl_name = Label(self.bottom, text= "Name", font='arial 15 bold' , fg='white', bg='#fcc324')
+        self.lbl_name.place(x=40,y=40)
+        self.ent_name = Entry(self.bottom, width=30, bd=4)
+        self.ent_name.insert(0,self.name)
+        self.ent_name.place(x=150,y=45)
+
+        #Surname
+        self.lbl_surname = Label(self.bottom, text= "Surname", font='arial 15 bold' , fg='white', bg='#fcc324')
+        self.lbl_surname.place(x=40,y=80)
+        self.ent_surname = Entry(self.bottom, width=30, bd=4)
+        self.ent_surname.insert(0,self.surname)
+        self.ent_surname.place(x=150,y=85)
+
+        #Email
+        self.lbl_email = Label(self.bottom, text= "Email", font='arial 15 bold' , fg='white', bg='#fcc324')
+        self.lbl_email.place(x=40,y=120)
+        self.ent_email = Entry(self.bottom, width=30, bd=4)
+        self.ent_email.insert(0,self.email)
+        self.ent_email.place(x=150,y=125)
+
+        #Phone
+        self.lbl_Phone = Label(self.bottom, text= "Phone", font='arial 15 bold' , fg='white', bg='#fcc324')
+        self.lbl_Phone.place(x=40,y=160)
+        self.ent_phone = Entry(self.bottom, width=30, bd=4)
+        self.ent_phone.insert(0,self.phone)
+        self.ent_phone.place(x=150,y=165)
+
+        #Address
+        self.lbl_address = Label(self.bottom, text= "Address", font='arial 15 bold' , fg='white', bg='#fcc324')
+        self.lbl_address.place(x=40,y=300)
+        self.ent_address = Text(self.bottom, width=23, height=15, wrap=WORD)
+        self.ent_address.insert('1.0',self.address)
+        self.ent_address.place(x=150,y=200)
+
+        button = Button(self.bottom, text='Update Person', command=self.updatePerson)
+        button.place(x=270,y=460)
+        self.lift()
+
+    def updatePerson(self):
+        person_id = self.id
+        name = self.ent_name.get()
+        surname =self.ent_surname.get()
+        email = self.ent_email.get()
+        phone = self.ent_phone.get()
+        address = self.ent_address.get(1.0,'end-1c')
+
+        try :
+            query = "update persons set person_name=?, person_surname=?, person_email=?, person_phone=?, person_address=? where person_id=?"
+            cur.execute(query,(name,surname,email,phone,address,person_id))
+            con.commit()
+            self.destroy()
+            messagebox.showinfo("Success","Info Updated Successfully",icon='info')
+            
+        
+        except:
+            messagebox.showerror("Failed","Info Not Updated",icon='error')
+            self.destroy()
+
+        
